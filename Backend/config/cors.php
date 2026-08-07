06a49b75845bca5dac2c733bcd5703d16473c3e4
unknown <nadjmi24ti@mahasiswa.pcr.ac.id>
2026-08-07 14:32:46 +0700

    deploy

--- Backend/config/cors.php
+++ Backend/config/cors.php
@@ -17,7 +17,9 @@ return [
 
     'allowed_methods' => ['*'],
 
-    'allowed_origins' => ['http://localhost:5173', 'http://localhost:3000', 'http://127.0.0.1:5173'],
+    'allowed_origins' => env('ALLOWED_ORIGINS')
+        ? explode(',', env('ALLOWED_ORIGINS'))
+        : ['http://localhost:5173', 'http://localhost:3000', 'http://127.0.0.1:5173'],
 
     'allowed_origins_patterns' => [],
 
